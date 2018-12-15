35d15ba4b69fd8b14b66a2d85678f8d66cd151fb
Popescu Robert Gabriel <robertpop99@yahoo.com>
2018-12-15 21:34:32 +0000

    finished stage 1

--- sketch/sketch.c
+++ sketch/sketch.c
@@ -3,43 +3,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#define byte unsigned char
 
 struct cursor{
-  unsigned int x;
-  unsigned int y;
-  unsigned int dx;
-  unsigned int dy;
-  unsigned int pen;
+ int x;   int y;
+ int dx;  int dy;
+ int pen;
 } cursor;
 
-byte getOp(byte b)
+int getOp(unsigned char b)
 {
   return ((0xC0 & b) >> 6);
 }
 
-byte getPos(byte b)
+int getPos(unsigned char b)
 {
-  return 0x3F & b;
+  int p = (0x0000001F & b);
+  if(0x20 & b) p -= 32;
+  return p;
 }
 
 void draw(display *d)
 {
-  line(d, cursor.x, cursor.y, cursor.dx, cursor.dy);
-  cursor.x = cursor.dx;
-  cursor.y = cursor.dy;
+  if(cursor.pen) line(d, cursor.x, cursor.y,
+                        cursor.x + cursor.dx, cursor.y + cursor.dy);
+  cursor.x += cursor.dx;
+  cursor.y += cursor.dy;
   cursor.dx = 0;
   cursor.dy = 0;
 }
 
-void run(display *d, byte b)
+void run(display *d, unsigned char b)
 {
-    byte op = getOp(b);
-    byte pos = getPos(b);
+    int op = getOp(b);
+    int pos = getPos(b);
     if(op == 0) cursor.dx += pos;
     if(op == 1)
     {
-      cursor.dy += pos;
+      cursor.dy = pos;
       draw(d);
     }
     if(op == 3) cursor.pen = !cursor.pen;
@@ -50,7 +50,7 @@ void input(char *file)
   display *d = newDisplay(file, 200, 200);
   cursor = (struct cursor) {0, 0, 0, 0, 0};
   FILE *in = fopen(file,"rb");
-  byte b = fgetc(in);
+  unsigned char b = fgetc(in);
   while(!feof(in))
   {
     run(d, b);
@@ -70,10 +70,10 @@ void testget()
   assert(getOp(0x7D) == 1); assert(getOp(0x44) == 1);
   assert(getOp(0x8F) == 2); assert(getOp(0xBC) == 2);
   assert(getOp(0xFF) == 3); assert(getOp(0xD0) == 3);
-  assert(getPos(0x03) == 3); assert(getPos(0x3D) == 61);
-  assert(getPos(0x7D) == 61); assert(getPos(0x44) == 4);
-  assert(getPos(0x8F) == 15); assert(getPos(0xBC) == 60);
-  assert(getPos(0xFF) == 63); assert(getPos(0xD0) == 16);
+  assert(getPos(0x03) == 3); assert(getPos(0x3D) == -3);
+  assert(getPos(0x7D) == -3); assert(getPos(0x44) == 4);
+  assert(getPos(0x8F) == 15); assert(getPos(0xBC) == -4);
+  assert(getPos(0xFF) == -1); assert(getPos(0xD0) == 16);
 }
 
 void testrun()
@@ -83,8 +83,8 @@ void testrun()
   assert(cursor.dx == 3);
   run(NULL,0xC0);
   assert(cursor.pen == 1);
-  run(NULL,0x3D);
-  assert(cursor.dx == 64);
+  run(NULL,0x3F);
+  assert(cursor.dx == 2);
   run(NULL,0xC0);
   assert(cursor.pen == 0);
 }
